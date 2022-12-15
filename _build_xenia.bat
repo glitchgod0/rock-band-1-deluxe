b42634daa2889876015f88b32f4ff48d8221f880
LysiX <smyler02@gmail.com>
2022-12-14 17:23:33 -0800

    logo

--- _build_xenia.bat
+++ _build_xenia.bat
@@ -1,4 +1,4 @@
 git pull https://github.com/hmxmilohax/rock-band-1-deluxe main
-"%~dp0\dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\Xbox\gen" -e
+"%~dp0\dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\Xbox\gen" -e -s 4073741823
 "%~dp0\_xenia\xenia_canary" "%~dp0\_build\xbox\default.xex"
 pause
