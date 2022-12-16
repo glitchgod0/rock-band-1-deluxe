4ec3921ed8e4fd5224e628e336e023f8969a37c7
LysiX <smyler02@gmail.com>
2022-12-15 21:11:26 -0800

    speedhack on end screen

--- _build_xenia.bat
+++ _build_xenia.bat
@@ -1,4 +1,5 @@
 git pull https://github.com/hmxmilohax/rock-band-1-deluxe main
+del "%~dp0\_build\Xbox\gen\main.hdr"
 del "%~dp0\_build\Xbox\gen\main_2.ark"
 xcopy "%~dp0\_build\_rebuild_files\main.hdr" "%~dp0\_build\Xbox\gen"
 "%~dp0dependencies/arkhelper" patchcreator -a "%~dp0\_ark" -o "%~dp0\_build\Xbox" "%~dp0\_build\Xbox\gen\main.hdr"
