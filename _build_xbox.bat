f2159cbffe53a855a3ae66a3a6f5f64fe6504def
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-12-24 01:47:13 -0600

    update build scripts with misinformation

--- _build_xbox.bat
+++ _build_xbox.bat
@@ -1,6 +1,12 @@
 git pull https://github.com/hmxmilohax/rock-band-1-deluxe main
+@echo OFF
+echo:Make sure your 1.0 vanilla ark files are in _build/Xbox/gen/
 del "%~dp0\_build\Xbox\gen\main.hdr"
 del "%~dp0\_build\Xbox\gen\main_2.ark"
-xcopy "%~dp0\_build\_rebuild_files\main.hdr" "%~dp0\_build\Xbox\gen"
-"%~dp0dependencies/arkhelper" patchcreator -a "%~dp0\_ark" -o "%~dp0\_build\Xbox" "%~dp0\_build\Xbox\gen\main.hdr"
+xcopy "%~dp0\_build\_rebuild_files\main.hdr" "%~dp0\_build\Xbox\gen" >nul
+echo:Building Rock Band 1 Deluxe patch arks.
+echo:The "Unhandled exception" below is expected, and does not indicate failure.
+"%~dp0dependencies/arkhelper" patchcreator -a "%~dp0\_ark" -o "%~dp0\_build\Xbox" "%~dp0\_build\Xbox\gen\main.hdr" >nul
+echo:Wrote Rock Band 1 Deluxe patch arks.
+echo:Complete. Enjoy Rock Band 1 Deluxe
 pause
