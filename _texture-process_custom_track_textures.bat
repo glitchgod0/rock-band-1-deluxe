4b4707c63a145ce40971ea714deaf06d44d47af8
LysiX <smyler02@gmail.com>
2022-12-28 19:06:29 -0800

    initial custom texture work, no flames

---
+++ _texture-process_custom_track_textures.bat
@@ -0,0 +1,5 @@
+cd "%~dp0custom_textures/custom_track_textures"
+FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0custom_textures\custom_track_textures/%%G" "%~dp0custom_textures\custom_track_textures\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.jpg') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0custom_textures\custom_track_textures/%%G" "%~dp0custom_textures\custom_track_textures\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.bmp') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0custom_textures\custom_track_textures/%%G" "%~dp0custom_textures\custom_track_textures\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/superfreq.exe" png2tex "%~dp0custom_textures\custom_track_textures/%%G" "%~dp0_ark\ui\track\custom_track_textures\gen\%%~nG.png_xbox" --platform x360 --miloVersion 25
