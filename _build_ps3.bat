12538a9b1e54b60ba5267aa0dd999839dbea61c9
Luna <luna.abarta@gmail.com>
2023-02-05 17:12:48 -0800

    rebuild files + updated scripts

--- _build_ps3.bat
+++ _build_ps3.bat
@@ -3,7 +3,7 @@ git pull https://github.com/hmxmilohax/rock-band-1-deluxe main
 echo:Make sure your 1.0 vanilla ark files are in _build/ps3/USRDIR/gen/
 del "%~dp0\_build\ps3\USRDIR\gen\main.hdr"
 del "%~dp0\_build\ps3\USRDIR\gen\main.ark"
-xcopy "%~dp0\_build\_rebuild_files\main.hdr" "%~dp0\_build\ps3\USRDIR\gen" >nul
+xcopy "%~dp0\_build\_rebuild_files\ps3\main.hdr" "%~dp0\_build\ps3\USRDIR\gen" >nul
 echo:Building Rock Band 1 Deluxe patch arks.
 echo:The "Unhandled exception" below is expected, and does not indicate failure.
 "%~dp0dependencies/arkhelper" patchcreator -a "%~dp0\_ark" -o "%~dp0\_build\ps3\USRDIR" "%~dp0\_build\ps3\USRDIR\gen\main.hdr" >nul
--- _build_xbox.bat
+++ _build_xbox.bat
@@ -3,7 +3,7 @@ git pull https://github.com/hmxmilohax/rock-band-1-deluxe main
 echo:Make sure your 1.0 vanilla ark files are in _build/Xbox/gen/
 del "%~dp0\_build\Xbox\gen\main.hdr"
 del "%~dp0\_build\Xbox\gen\main_2.ark"
-xcopy "%~dp0\_build\_rebuild_files\main.hdr" "%~dp0\_build\Xbox\gen" >nul
+xcopy "%~dp0\_build\_rebuild_files\xbox\main.hdr" "%~dp0\_build\Xbox\gen" >nul
 echo:Building Rock Band 1 Deluxe patch arks.
 echo:The "Unhandled exception" below is expected, and does not indicate failure.
 "%~dp0dependencies/arkhelper" patchcreator -a "%~dp0\_ark" -o "%~dp0\_build\Xbox" "%~dp0\_build\Xbox\gen\main.hdr" >nul
