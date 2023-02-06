f666cd8e7c3f9626cabbf708629275a3e6f86fc4
Luna <luna.abarta@gmail.com>
2023-02-05 17:05:35 -0800

    ps3 attempt 2

--- _build_ps3.bat
+++ _build_ps3.bat
@@ -1,23 +1,12 @@
-set FAILED_ARK_BUILD=0
 git pull https://github.com/hmxmilohax/rock-band-1-deluxe main
-@echo off
-echo:
-echo:Temporarily moving Xbox files out of the ark path to reduce final ARK size
-@%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.milo_xbox /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.png_xbox /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.bmp_xbox /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-echo:
-echo:Building PS3 ARK
-"%~dp0dependencies/arkhelper" dir2ark "%~dp0_ark" "%~dp0_build\ps3\USRDIR\gen" -n "patch_ps3" -e -v 5 >nul
-if %errorlevel% neq 0 (set FAILED_ARK_BUILD=1)
-echo:
-echo:Moving back Xbox files
-@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0_ark" *.milo_xbox /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0_ark" *.png_xbox /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0_ark" *.bmp_xbox /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-rd _temp
-echo:
-if %FAILED_ARK_BUILD% neq 1 (echo:Successfully built Rock Band 1 Deluxe ARK. You may find the files needed to place on your PS3 in /_build/PS3/)
-if %FAILED_ARK_BUILD% neq 0 (echo:Error building ARK. Check your modifications or run _git_reset.bat to rebase your repo.)
-echo:
+@echo OFF
+echo:Make sure your 1.0 vanilla ark files are in _build/ps3/USRDIR/gen/
+del "%~dp0\_build\ps3\USRDIR\gen\main_ps3.hdr"
+del "%~dp0\_build\ps3\USRDIR\gen\main_ps3_2.ark"
+xcopy "%~dp0\_build\_rebuild_files\main_ps3.hdr" "%~dp0\_build\ps3\USRDIR\gen" >nul
+echo:Building Rock Band 1 Deluxe patch arks.
+echo:The "Unhandled exception" below is expected, and does not indicate failure.
+"%~dp0dependencies/arkhelper" patchcreator -a "%~dp0\_ark" -o "%~dp0\_build\ps3\USRDIR" "%~dp0\_build\ps3\USRDIR\gen\main_ps3.hdr" >nul
+echo:Wrote Rock Band 1 Deluxe patch arks.
+echo:Complete. Enjoy Rock Band 1 Deluxe
 pause
